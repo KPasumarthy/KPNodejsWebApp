06721688a2790188dfcd681084aeb67e445617fd
Kailash N Pasumarthy <sfknp@wakefern.com>
2018-10-18 14:13:44 -0400

    KP : ngRoute app-routing Module added

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+//import { CommonModule } from '@angular/common'; //KP : Auto-Generated using <ng generate module app-routing --flat --module=app>
+/** KP : Additonal Router Modules to be generated */
+import { RouterModule, Routes } from '@angular/router';
+
+
+@NgModule({
+  imports: [
+    //CommonModule
+  ],
+  exports: [
+    RouterModule
+  ],
+  declarations: []
+})
+export class AppRoutingModule { }
